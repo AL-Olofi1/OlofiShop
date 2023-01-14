890a9abb009c5aa9ab3cf9c45780e41d6f5f6479
alolofi <alolofi@uni-bremen.de>
2023-01-14 19:44:52 +0100

    after adding the header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
+import {Container} from 'react-bootstrap';  
 import logo from './logo.svg';
-import './App.css';
+import MyNavbar from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      
+      <MyNavbar />
+      <main className='py-6'>
+      <Container>
+
+      <h1>My App</h1>
+
+      </Container>
+      </main>
+      <Footer />
+
     </div>
   );
 }
