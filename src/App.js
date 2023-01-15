1ae8fbf71c683674a34e315c9c07951395f6bae6
alolofi <alolofi@uni-bremen.de>
2023-01-15 23:27:23 +0100

    added Login

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import MyNavbar from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './components/HomeScreen';
 import ProductScreen from './components/ProductScreen';
-import { BrowserRouter as Router, Routes, Route , Link } from 'react-router-dom';
+import Login from './components/Login';
+import Cart from './components/Cart';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom'
 
 function App() {
   return (
@@ -11,10 +14,13 @@ function App() {
       <MyNavbar />
       <main className='py-6'>
         <Container>
-          <Routes> 
-            <Route path='/' element={<HomeScreen />} exact /> 
-            <Route path='/product/:id' element={<ProductScreen />} exact /> 
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/cart' element={<Cart />} />
           </Routes>
+
         </Container>
       </main>
       <Footer />
